217c36ce00e28d8300cbfab1a1b086fdb14ac3e8
Sinoed <cnooed@gmail.com>
2019-03-04 11:07:15 +0330

    Added needed methods to Book Class

--- src/main/java/com/sinoed/library/domain/Book.java
+++ src/main/java/com/sinoed/library/domain/Book.java
@@ -9,4 +9,54 @@ public class Book {
     private String libraryName;
 
     private String publisher;
+
+    public Book(String name, String libraryName, String publisher) {
+        this.name = name;
+        this.libraryName = libraryName;
+        this.publisher = publisher;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLibraryName(String libraryName) {
+        this.libraryName = libraryName;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLibraryName() {
+        return libraryName;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
 }
