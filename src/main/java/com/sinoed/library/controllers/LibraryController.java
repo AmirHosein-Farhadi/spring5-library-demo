7c89e61dcb5347f5fb0cb7ce172738f13153b029
Sinoed <cnooed@gmail.com>
2019-03-04 09:48:59 +0330

    configured Controller to show index page

--- src/main/java/com/sinoed/library/controllers/LibraryController.java
+++ src/main/java/com/sinoed/library/controllers/LibraryController.java
@@ -1,8 +1,13 @@
 package com.sinoed.library.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LibraryController {
 
+    @RequestMapping({"","/"})
+    public String getIndex(){
+        return "index";
+    }
 }
