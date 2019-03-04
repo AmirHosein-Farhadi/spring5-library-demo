e3c80fc61d48fb79f4a93a9c5e176d771b0a1c29
Sinoed <cnooed@gmail.com>
2019-03-04 11:05:09 +0330

    Added needed methods to Author Class

--- src/main/java/com/sinoed/library/domain/Author.java
+++ src/main/java/com/sinoed/library/domain/Author.java
@@ -11,4 +11,54 @@ public class Author {
     private String lastName;
 
     private Set<Book> books = new HashSet<>();
+
+    public Author(String name, String lastName, Set<Book> books) {
+        this.name = name;
+        this.lastName = lastName;
+        this.books = books;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
 }
