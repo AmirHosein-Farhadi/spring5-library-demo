35eb9bd865624ed2875069c3e715fafa5a52993b
Sinoed <cnooed@gmail.com>
2019-03-11 11:04:57 +0330

    refactored Book and Author using Lombok

--- src/main/java/com/sinoed/library/domain/Author.java
+++ src/main/java/com/sinoed/library/domain/Author.java
@@ -1,10 +1,14 @@
 package com.sinoed.library.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
 @Entity
 public class Author {
 
@@ -19,9 +23,6 @@ public class Author {
     @ManyToMany
     private Set<Book> books = new HashSet<>();
 
-    public Author() {
-    }
-
     public Author(String name, String lastName) {
         this.name = name;
         this.lastName = lastName;
@@ -33,55 +34,4 @@ public class Author {
         this.books = books;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setBooks(Set<Book> books) {
-        this.books = books;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Set<Book> getBooks() {
-        return books;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Author author = (Author) o;
-
-        return Objects.equals(id, author.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
-    @Override
-    public String toString() {
-        return "Author{" +
-                "name='" + name + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", books=" + books +
-                '}';
-    }
 }
--- src/main/java/com/sinoed/library/domain/Book.java
+++ src/main/java/com/sinoed/library/domain/Book.java
@@ -1,10 +1,12 @@
 package com.sinoed.library.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
+@Data
 @Entity
 public class Book {
 
@@ -20,72 +22,4 @@ public class Book {
 
     @ManyToMany
     private Set<Author> authors = new HashSet<>();
-
-    public Book() {
-    }
-
-    public Book(String name, String libraryName, String publisher, Set<Author> authors) {
-        this.name = name;
-        this.libraryName = libraryName;
-        this.publisher = publisher;
-        this.authors = authors;
-    }
-
-    public Book(String name, String libraryName, String publisher) {
-        this.name = name;
-        this.libraryName = libraryName;
-        this.publisher = publisher;
-    }
-
-    public Set<Author> getAuthors() {
-        return authors;
-    }
-
-    public void setAuthors(Set<Author> authors) {
-        this.authors = authors;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setLibraryName(String libraryName) {
-        this.libraryName = libraryName;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getLibraryName() {
-        return libraryName;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Book book = (Book) o;
-
-        return Objects.equals(id, book.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return id != null ? id.hashCode() : 0;
-    }
-
 }
